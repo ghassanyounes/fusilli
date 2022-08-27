a106f5526da1577ca6f01c36ea992df7365d4923
Ghassan Younes <22338451+ghassanyounes@users.noreply.github.com>
2022-08-26 20:33:47 -0700

    Initial Commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
+#include <exception>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+#include <SDL2pp/SDL2pp.hh>
+
+using std::cerr, std::cout, std::endl;
+
+int main() try {
+    // Init SDL
+    SDL2pp::SDL sdl(SDL_INIT_VIDEO);
+
+    SDL2pp::Window window("Fusilli", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_RESIZABLE);
+
+    SDL2pp::Renderer renderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    // Load sprites image as a new texture
+    SDL2pp::Texture sprites(renderer, "/M484SpaceSoldier.png");
+
+    // Clear screen
+    renderer.Clear();
+
+    // Render our image, stretching it to the whole window
+    renderer.Copy(sprites);
+
+    // Show rendered frame
+    renderer.Present();
+
+    // 5 second delay
+    SDL_Delay(5000);
+
+    // All resources should be automatically released and the library should de-initialize.
     return 0;
+} catch (std::exception &e) {
+    // In the event of an error, print it and exit.
+    cerr << e.what() << endl;
 }
